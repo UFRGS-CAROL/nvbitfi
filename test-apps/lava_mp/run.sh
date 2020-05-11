d2dd7530f52afe806746a6755e694e0285ce1f32
Your Name <you@example.com>
2020-05-10 22:17:31 -0300

    update lava mp

--- test-apps/lava_mp/run.sh
+++ test-apps/lava_mp/run.sh
@@ -5,7 +5,10 @@ DMR=none
 SIZE=2 #3
 PRECISION=single
 DATADIR=/home/carol/radiation-benchmarks/data/lava
-#echo ${PRELOAD_FLAG}
-#PRELOAD_FLAG="LD_PRELOAD=/home/carol/NVBITFI/nvbit_release/tools/nvbitfi/injector/injector.so"
-#BIN_DIR=./
-eval ${PRELOAD_FLAG} ${BIN_DIR}/cuda_lava -boxes ${SIZE} -streams 1 -iterations 1 -input_distances ${DATADIR}/lava_${PRECISION}_distances_${SIZE} -input_charges ${DATADIR}/lava_${PRECISION}_charges_${SIZE} -output_gold ${DATADIR}/lava_${PRECISION}_gold_${SIZE} -opnum ${OPNUM} -precision ${PRECISION} -redundancy ${DMR} > stdout.txt 2> stderr.txt
+
+eval ${PRELOAD_FLAG} ${BIN_DIR}/cuda_lava -boxes ${SIZE} -streams 1 -iterations 1 -input_distances ${DATADIR}/lava_${PRECISION}_distances_${SIZE} -input_charges ${DATADIR}/lava_${PRECISION}_charges_${SIZE} -output_gold ${DATADIR}/lava_${PRECISION}_gold_${SIZE} -opnum ${OPNUM} -precision ${PRECISION} -redundancy ${DMR} -verbose > stdout.txt 2> stderr.txt
+sed -i '/LOGFILENAME/c\' stdout.txt 
+sed -i '/time/c\' stdout.txt 
+sed -i '/TESTE/c\' stdout.txt 
+sed -i '/========/c\' stdout.txt
+
