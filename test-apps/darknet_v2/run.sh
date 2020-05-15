116262336b8cbd9c061de3c60d9b7e1e0eb241d9
Your Name <you@example.com>
2020-05-15 20:58:21 -0300

    update on gaussian

--- test-apps/darknet_v2/run.sh
+++ test-apps/darknet_v2/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-RADDIR=/home/carol/radiation-benchmarks/data/darknet
+RADDIR=/home/carol/radiation-benchmarks
 
+echo ${PRELOAD_FLAG}
 
-eval ${PRELOAD_FLAG}  ${BIN_DIR}/darknet_v2 test_radiation -d ${RAD_DIR}/data/darknet/fault_injection.csv -n 1 -s 0 -a 0 > stdout.txt 2> stderr.txt
+eval ${PRELOAD_FLAG}  ${BIN_DIR}/darknet_v2 test_radiation -d ${RADDIR}/data/darknet/fault_injection.csv -n 1 -s 0 -a 0 > stdout.txt 2> stderr.txt
 sed -i '/Iteration 0 - image/c\' stdout.txt 
 sed -i '/^$/d' stdout.txt
 
