ed4fc8c7bbb7a4c0869faede3d6eb8f91b83bee7
carol <carol@caroltivanv2.inf.ufrgs.br>
2020-07-09 02:28:21 +0100

    update on darknet

--- test-apps/darknet_v2/run.sh
+++ test-apps/darknet_v2/run.sh
@@ -2,7 +2,7 @@
 
 RADDIR=/home/carol/radiation-benchmarks
 
-echo ${PRELOAD_FLAG}
+#echo ${PRELOAD_FLAG}
 
 eval ${PRELOAD_FLAG}  ${BIN_DIR}/darknet_v2 test_radiation -d ${RADDIR}/data/darknet/fault_injection.csv -n 1 -s 0 -a 0 > stdout.txt 2> stderr.txt
 sed -i '/Iteration 0 - image/c\' stdout.txt 
