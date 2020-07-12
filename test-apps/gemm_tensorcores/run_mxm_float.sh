3722c3ff90b4b876225d087c68eb2822a76e091d
Your Name <you@example.com>
2020-07-13 00:20:25 +0100

    gemm multiprecision fi

---
+++ test-apps/gemm_tensorcores/run_mxm_float.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+OPNUM=1
+DMR=none
+ALPHA=1.0
+BETA=0.0
+SIZE=256
+PRECISION=float
+DATADIR=/home/carol/radiation-benchmarks/data/gemm
+
+
+eval ${PRELOAD_FLAG} ${BIN_DIR}/gemm --size ${SIZE} --precision ${PRECISION} --dmr ${DMR} --iterations 1 --alpha ${ALPHA} --beta ${BETA} --input_a ${DATADIR}/a_float_${ALPHA}_${BETA}_${SIZE}_cublas_0_tensor_0.matrix --input_b ${DATADIR}/b_float_${ALPHA}_${BETA}_${SIZE}_cublas_0_tensor_0.matrix --input_c ${DATADIR}/c_float_${ALPHA}_${BETA}_${SIZE}_cublas_0_tensor_0.matrix  --gold ${DATADIR}/g_float_${ALPHA}_${BETA}_${SIZE}_cublas_0_tensor_0.matrix --opnum ${OPNUM} --verbose > stdout.txt 2> stderr.txt
+
+sed -i '/LOGFILENAME/c\' stdout.txt 
+sed -i '/Time/c\' stdout.txt 
+sed -i '/time/c\' stdout.txt 
+sed -i '/^$/d' stdout.txt 
