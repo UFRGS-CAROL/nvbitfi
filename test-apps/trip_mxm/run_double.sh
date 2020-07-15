b6f8b68e55b3f6715412665b4c37e445bd3ac7b0
carol <fernandofernandesantos@gmail.com>
2020-07-15 17:47:38 +0000

    run double update

--- test-apps/trip_mxm/run_double.sh
+++ test-apps/trip_mxm/run_double.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-SIZE=1024
+SIZE=256
 DATADIR=/home/carol/radiation-benchmarks/data/gemm
 PRECISION=double
 
-eval ${PRELOAD_FLAG} ${BIN_DIR}/cuda_trip_mxm_${PRECISION} -size=${SIZE} -input_a=${DATADIR}/mxm_${PRECISION}_A_${SIZE}.matrix -input_b=${DATADIR}/mxm_${PRECISION}_B_${SIZE}.matrix -gold=${DATADIR}/mxm_${PRECISION}_GOLD_.matrix -iterations=1 -verbose -no-warmup > stdout.txt 2> stderr.txt
+eval ${PRELOAD_FLAG} ${BIN_DIR}/cuda_trip_mxm_${PRECISION} -size=${SIZE} -input_a=${DATADIR}/mxm_${PRECISION}_A_8192.matrix -input_b=${DATADIR}/mxm_${PRECISION}_B_8192.matrix -gold=${DATADIR}/mxm_${PRECISION}_GOLD_.matrix  -iterations=1 -verbose -no-warmup > stdout.txt 2> stderr.txt
 sed -i '/Preparing/c\' stdout.txt
 sed -i '/Average/c\' stdout.txt
 sed -i '/time/c\' stdout.txt
