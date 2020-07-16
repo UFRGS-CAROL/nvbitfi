eb81365b2fb7f48aee7e5c513c75237ccebd6531
carol <fernandofernandesantos@gmail.com>
2020-07-16 03:50:10 +0000

    lava float

--- test-apps/trip_lava/run_float.sh
+++ test-apps/trip_lava/run_float.sh
@@ -2,7 +2,7 @@
 
 SIZE=2
 DATADIR=/home/carol/radiation-benchmarks/data/lava
-PRECISION=double
+PRECISION=single
 
 eval ${PRELOAD_FLAG} ${BIN_DIR}/cuda_trip_lava_${PRECISION} -boxes=${SIZE} -streams=1 -iterations=1 -verbose -input_distances=${DATADIR}/lava_${PRECISION}_distances_${SIZE} -input_charges=${DATADIR}/lava_${PRECISION}_charges_${SIZE} -output_gold=${DATADIR}/lava_${PRECISION}_gold_${SIZE}  > stdout.txt 2> stderr.txt
 sed -i '/Iteration/c\' stdout.txt
