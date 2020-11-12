a914fe1f5490bdb273b6bd0ce6c448a36048d724
Your Name <you@example.com>
2020-11-12 13:37:44 -0300

    update on darknet

--- test-apps/accl/run.sh
+++ test-apps/accl/run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+set -x
 RADD=/home/carol/radiation-benchmarks/data/accl
+PRELOAD_FLAG=/home/carol/NVBITFI/nvbit_release/tools/nvbitfi/profiler/profiler.so
+BIN_DIR=.
 
 #${BIN_DIR}/accl 2 1  ${APP_DIR}/2Frames.pgm ${APP_DIR}/GOLD_2Frames 1 -verbose > stdout.txt 2> stderr.txt 
 eval ${PRELOAD_FLAG}  ${BIN_DIR}/cudaACCL --size 7 --frames 7 --input ${RADD}/7Frames.pgm --gold ${RADD}/gold_7_7.data --iterations 1 --verbose  > stdout.txt 2>stderr.txt
