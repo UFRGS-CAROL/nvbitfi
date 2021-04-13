93ddc388f31f79b646ddbf4c7239365d04c85c1d
Fernando <fernandofernandesantos@gmail.com>
2021-04-13 17:24:14 -0300

    update on run mxm

--- test-apps/gemm/run_mxm_float.sh
+++ test-apps/gemm/run_mxm_float.sh
@@ -6,7 +6,7 @@ ALPHA=1.0
 BETA=0.0
 SIZE=256
 PRECISION=float
-DATADIR=/home/carol/radiation-benchmarks/data/gemm
+DATADIR=/home/fernando/radiation-benchmarks/data/gemm
 
 
 eval ${PRELOAD_FLAG} ${BIN_DIR}/gemm --size ${SIZE} --precision ${PRECISION} --dmr ${DMR} --iterations 1 --alpha ${ALPHA} --beta ${BETA} --input_a ${DATADIR}/a_float_${ALPHA}_${BETA}_${SIZE}_cublas_0_tensor_0.matrix --input_b ${DATADIR}/b_float_${ALPHA}_${BETA}_${SIZE}_cublas_0_tensor_0.matrix --input_c ${DATADIR}/c_float_${ALPHA}_${BETA}_${SIZE}_cublas_0_tensor_0.matrix  --gold ${DATADIR}/g_float_${ALPHA}_${BETA}_${SIZE}_cublas_0_tensor_0.matrix --opnum ${OPNUM} --verbose > stdout.txt 2> stderr.txt
