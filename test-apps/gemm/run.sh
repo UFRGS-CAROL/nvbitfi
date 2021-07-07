6fa734684190cb99145be0a1e37239cb456ac159
Fernando Fernandes <fernandofernandesantos@gmail.com>
2021-07-07 01:44:59 -0300

    Update parameters for run.sh

--- test-apps/gemm/run.sh
+++ test-apps/gemm/run.sh
@@ -7,14 +7,19 @@ BETA=0.0
 SIZE=256
 DATADIR=/home/fernando/radiation-benchmarks/data/gemm
 
+CUBLAS=0
 PRECISION=float
 CUDAPATH=/usr/local/cuda
 if [ $# -gt 0 ]; then
   PRECISION=$1
   CUDAPATH=$2
+  CUBLAS=$3
+  if [ $CUBLAS -eq 1 ]; then
+    USECUBLAS=--use_cublas
+  fi
 fi
 
-eval LD_LIBRARY_PATH=${CUDAPATH}:$LD_LIBRARY_PATH ${PRELOAD_FLAG} ${BIN_DIR}/gemm --size ${SIZE} --precision ${PRECISION} --dmr ${DMR} --iterations 1 --alpha ${ALPHA} --beta ${BETA} --input_a ${DATADIR}/a_float_${ALPHA}_${BETA}_${SIZE}_cublas_0_tensor_0.matrix --input_b ${DATADIR}/b_float_${ALPHA}_${BETA}_${SIZE}_cublas_0_tensor_0.matrix --input_c ${DATADIR}/c_float_${ALPHA}_${BETA}_${SIZE}_cublas_0_tensor_0.matrix --gold ${DATADIR}/g_float_${ALPHA}_${BETA}_${SIZE}_cublas_0_tensor_0.matrix --opnum ${OPNUM} --verbose >stdout.txt 2>stderr.txt
+eval LD_LIBRARY_PATH=${CUDAPATH}:$LD_LIBRARY_PATH ${PRELOAD_FLAG} ${BIN_DIR}/gemm ${USECUBLAS} --size ${SIZE} --precision ${PRECISION} --dmr ${DMR} --iterations 1 --alpha ${ALPHA} --beta ${BETA} --input_a ${DATADIR}/a_float_${ALPHA}_${BETA}_${SIZE}_cublas_${CUBLAS}_tensor_0.matrix --input_b ${DATADIR}/b_float_${ALPHA}_${BETA}_${SIZE}_cublas_${CUBLAS}_tensor_0.matrix --input_c ${DATADIR}/c_float_${ALPHA}_${BETA}_${SIZE}_cublas_${CUBLAS}_tensor_0.matrix --gold ${DATADIR}/g_float_${ALPHA}_${BETA}_${SIZE}_cublas_${CUBLAS}_tensor_0.matrix --opnum ${OPNUM} --verbose >stdout.txt 2>stderr.txt
 
 sed -i '/LOGFILENAME/c\' stdout.txt
 sed -i '/Time/c\' stdout.txt
