634555801ce651b95427b56b07976b15df2e63c7
Fernando Fernandes <fernandofernandesantos@gmail.com>
2021-07-09 12:29:30 -0300

    Update parameters for permanent_fault_injector.py

---
+++ pf_injector/parse_injection_files.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3.8
+
+import glob
+import logging
+
+import pandas as pd
+
+from commom import execute_cmd, OPCODES
+
+
+def untar_and_process_files():
+    tar_files = glob.glob("data/*.tar.gz")
+    output_folder = "/tmp/pf_faults"
+    execute_cmd(f"mkdir -p {output_folder}")
+    final_list = list()
+    for file in tar_files:
+        untar_cmd = f"tar xzf {file} -C {output_folder}"
+        execute_cmd(untar_cmd)
+        text_file = f"{output_folder}/{file.replace('.tar.gz', '.txt').replace('data/', '')}"
+        nvbit_file = f"{output_folder}/nvbitfi-injection-info.txt"
+        with open(text_file) as fault_output_file, open(nvbit_file) as nvbit_fp:
+            sm_id, lane_id, mask, opcode = nvbit_fp.readlines()
+            sdc, due = 0, 1
+            # TODO: THIS ONLY WORK FOR MXM
+            for line in fault_output_file:
+                if "Result = FAIL" in line:
+                    sdc = 1
+                if "done" in line:
+                    due = 0
+
+            final_list.append({
+                "sm_id": int(sm_id.strip()), "lane_id": int(lane_id.strip()), "mask": bin(int(mask.strip())),
+                "opcode": OPCODES[int(opcode)], "SDC": sdc, "DUE": due
+            })
+
+    return pd.DataFrame(final_list)
+
+
+def main():
+    logging.basicConfig(level=logging.DEBUG,
+                        format='[%(asctime)s][%(levelname)s][%(filename)s:%(lineno)d] %(message)s',
+                        datefmt='%m/%d/%Y %H:%M:%S')
+
+    df = untar_and_process_files()
+    df_sum = df.groupby(["sm_id", "lane_id", "opcode"]).sum()
+    print(df_sum)
+    df_sum.to_csv("final_result_mxm.csv")
+
+
+if __name__ == '__main__':
+    main()
