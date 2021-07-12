a0806804404dd9ae5160fbd3db691423721b1df4
Fernando Fernandes <fernandofernandesantos@gmail.com>
2021-07-11 22:59:40 -0300

    Update automate hotspot fault injection

--- test-apps/hotspot/run.sh
+++ test-apps/hotspot/run.sh
@@ -15,7 +15,7 @@ fi
 
 RADDIR=/home/carol/radiation-benchmarks/data/hotspot
 
-eval LD_LIBRARY_PATH=${CUDAPATH}/lib64:$LD_LIBRARY_PATH ${PRELOAD_FLAG} ${BIN_DIR}/cuda_hotspot -verbose -size=${SIZE} -sim_time=${SIM_TIME} -streams=${STREAMS} -input_temp=${RADDIR}/temp_${SIZE} -input_power=${RADDIR}/power_${SIZE} -gold_temp=${RADDIR}/gold_${SIZE}_${SIM_TIME} -iterations=1 > stdout.txt 2> stderr.txt
+eval LD_LIBRARY_PATH=${CUDAPATH}/lib64:$LD_LIBRARY_PATH ${PRELOAD_FLAG} ${BIN_DIR}/cuda_hotspot -verbose -size=${SIZE} -sim_time=${SIM_TIME} -streams=${STREAMS} -input_temp=${RADDIR}/temp_${SIZE} -input_power=${RADDIR}/power_${SIZE} -gold_temp=${RADDIR}/gold_${PRECISION}_${SIZE}_${SIM_TIME} -iterations=1 > stdout.txt 2> stderr.txt
 sed -i '/LOGFILENAME/c\' stdout.txt 
 sed -i '/Time/c\' stdout.txt 
 sed -i '/time/c\' stdout.txt 
