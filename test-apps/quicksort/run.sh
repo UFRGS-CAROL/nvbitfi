950d9e711c988819c9156075d120d99ad433466a
Fernando Fernandes <fernandofernandesantos@gmail.com>
2021-07-13 01:12:49 -0300

    Update automate hotspot fault injection

--- test-apps/quicksort/run.sh
+++ test-apps/quicksort/run.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-RADDIR=/home/carol/radiation-benchmarks/data/quicksort
+RADDIR=/home/fernando/radiation-benchmarks/data/quicksort
 
-SIZE=1048576 
+SIZE=1048576
+CUDAPATH=/usr/local/cuda
+if [ $# -gt 0 ]; then
+  CUDAPATH=$1
+  SIZE=$2
+fi
 
-eval ${PRELOAD_FLAG} ${BIN_DIR}/quicksort -size=${SIZE} -input=${RADDIR}/quicksort_input_134217728 -gold=${RADDIR}/quicksort_gold_${SIZE} -iterations=1 -verbose > stdout.txt 2> stderr.txt
+eval LD_LIBRARY_PATH=${CUDAPATH}/lib64:$LD_LIBRARY_PATH ${PRELOAD_FLAG} ${BIN_DIR}/quicksort -size=${SIZE} -input=${RADDIR}/quicksort_input_134217728 -gold=${RADDIR}/quicksort_gold_${SIZE} -iterations=1 -verbose > stdout.txt 2> stderr.txt
 sed -i '/LOGFILENAME/c\' stdout.txt 
 sed -i '/Time/c\' stdout.txt 
 sed -i '/time/c\' stdout.txt 
