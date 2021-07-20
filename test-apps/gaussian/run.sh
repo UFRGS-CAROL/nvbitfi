5a8c6aba81c32ed7a732eaac5d21654484a76685
Fernando Fernandes <fernandofernandesantos@gmail.com>
2021-07-20 19:20:55 -0300

    Multiple fault injection at the same time

--- test-apps/gaussian/run.sh
+++ test-apps/gaussian/run.sh
@@ -2,10 +2,15 @@
 
 RADDIR=/home/carol/radiation-benchmarks
 DIR=${RADDIR}/data/gaussian
-
+CUDAPATH=/usr/local/cuda
 SIZE=512
+if [ $# -gt 0 ]; then
+  CUDAPATH=$1
+  SIZE=$2
+fi
 
-eval ${PRELOAD_FLAG} ${APP_DIR}/cudaGaussian --size ${SIZE} --input ${RADDIR}/input_${SIZE}.data --gold ${RADDIR}/gold_${SIZE}.data --iterations 1 --verbose > stdout.txt 2>stderr.txt
+
+eval LD_LIBRARY_PATH=${CUDAPATH}/lib64:$LD_LIBRARY_PATH  ${PRELOAD_FLAG} ${APP_DIR}/cudaGaussian --size ${SIZE} --input ${RADDIR}/input_${SIZE}.data --gold ${RADDIR}/gold_${SIZE}.data --iterations 1 --verbose > stdout.txt 2>stderr.txt
 sed -i '/time/c\' stdout.txt 
 sed -i '/LOGFILENAME/c\' stdout.txt
 sed -i '/^$/d' stdout.txt
--- test-apps/lava_mp/run.sh
+++ test-apps/lava_mp/run.sh
@@ -5,6 +5,7 @@ DMR=none
 SIZE=2 #3
 PRECISION=single
 CUDAPATH=/usr/local/cuda
+STREAMS=1
 if [ $# -gt 0 ]; then
   CUDAPATH=$1
   PRECISION=$2
@@ -14,7 +15,7 @@ fi
 
 DATADIR=/home/carol/radiation-benchmarks/data/lava
 
-eval  LD_LIBRARY_PATH=${CUDAPATH}/lib64:$LD_LIBRARY_PATH  ${PRELOAD_FLAG} ${BIN_DIR}/cuda_lava -boxes ${SIZE} -streams 1 -iterations 1 -input_distances ${DATADIR}/lava_${PRECISION}_distances_${SIZE} -input_charges ${DATADIR}/lava_${PRECISION}_charges_${SIZE} -output_gold ${DATADIR}/lava_${PRECISION}_gold_${SIZE} -opnum ${OPNUM} -precision ${PRECISION} -redundancy ${DMR} -verbose >stdout.txt 2>stderr.txt
+eval  LD_LIBRARY_PATH=${CUDAPATH}/lib64:$LD_LIBRARY_PATH  ${PRELOAD_FLAG} ${BIN_DIR}/cuda_lava -boxes ${SIZE} -streams ${STREAMS} -iterations 1 -input_distances ${DATADIR}/lava_${PRECISION}_distances_${SIZE} -input_charges ${DATADIR}/lava_${PRECISION}_charges_${SIZE} -output_gold ${DATADIR}/lava_${PRECISION}_gold_${SIZE} -opnum ${OPNUM} -precision ${PRECISION} -redundancy ${DMR} -verbose >stdout.txt 2>stderr.txt
 sed -i '/LOGFILENAME/c\' stdout.txt
 sed -i '/time/c\' stdout.txt
 sed -i '/TESTE/c\' stdout.txt
