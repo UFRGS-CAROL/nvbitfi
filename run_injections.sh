0d40da96d4d1659500cbd32f25a7c1a81fb8dd7e
Fernando Fernandes <fernandofernandesantos@gmail.com>
2021-07-28 13:04:07 -0300

    Fit prediction

--- run_injections.sh
+++ run_injections.sh
@@ -3,8 +3,14 @@
 # stop after first error
 set -e
 
+if [ $# -ne 4 ]; then
+  echo "$0 <BENCHMARK> <FAULTS> <ADDITIONAL PARAMETERS LIST between \"\">"
+  exit 1
+fi
+
 export BENCHMARK=$1
 export FAULTS=$2
 export ADDITIONAL_PARAMETERS=$3
 
 ./test.sh "$1"
+exit 0
