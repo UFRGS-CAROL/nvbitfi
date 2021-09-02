cc69c3c798bab20c4a05981adadbafa7bd9e79bf
Fernando <fernandofernandesantos@gmail.com>
2021-09-02 01:16:32 +0000

    update on run and make

--- test-apps/cfd/run.sh
+++ test-apps/cfd/run.sh
@@ -7,8 +7,8 @@ fi
 RADDIR=/home/carol/radiation-benchmarks
 DATA=${RADDIR}/data/cfd
 INPUT_BASE=missile.domn.0.2M
-INPUT=${DATADIR}/${INPUT_BASE}
-GOLD=${DATADIR}/cfd_gold_${INPUT_BASE}
+INPUT=${DATA}/${INPUT_BASE}
+GOLD=${DATA}/cfd_gold_${INPUT_BASE}
 
 eval LD_LIBRARY_PATH=${CUDAPATH}/lib64:$LD_LIBRARY_PATH ${PRELOAD_FLAG} ${BIN_DIR}/cudaCFD --streams 1 --input ${INPUT} --gold ${GOLD} --iterations 1 --verbose > stdout.txt 2> stderr.txt
 
