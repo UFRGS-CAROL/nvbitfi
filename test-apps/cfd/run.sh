12f7102beaf316cf2952cd40b1bcdec3d50e40d6
Fernando Fernandes <fernandofernandesantos@gmail.com>
2021-09-01 22:57:12 -0300

    Adding CFD

--- test-apps/cfd/run.sh
+++ test-apps/cfd/run.sh
@@ -2,6 +2,7 @@
 
 if [ $# -gt 0 ]; then
   CUDAPATH=$1
+  STREAMS=$2
 fi
 
 RADDIR=/home/carol/radiation-benchmarks
@@ -10,7 +11,7 @@ INPUT_BASE=missile.domn.0.2M
 INPUT=${DATA}/${INPUT_BASE}
 GOLD=${DATA}/cfd_gold_${INPUT_BASE}
 
-eval LD_LIBRARY_PATH=${CUDAPATH}/lib64:$LD_LIBRARY_PATH ${PRELOAD_FLAG} ${BIN_DIR}/cudaCFD --streams 1 --input ${INPUT} --gold ${GOLD} --iterations 1 --verbose > stdout.txt 2> stderr.txt
+eval LD_LIBRARY_PATH=${CUDAPATH}/lib64:$LD_LIBRARY_PATH ${PRELOAD_FLAG} ${BIN_DIR}/cudaCFD --streams ${STREAMS} --input ${INPUT} --gold ${GOLD} --iterations 1 --verbose > stdout.txt 2> stderr.txt
 
 sed -i '/Kernel time/c\REPLACED.' stdout.txt
 sed -i '/LOGFILE/c\REPLACED.' stdout.txt 
