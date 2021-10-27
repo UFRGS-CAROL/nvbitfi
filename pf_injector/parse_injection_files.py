6b36ddf24802d22111cacb7caf8ab4cb2e5dfeb0
Fernando Fernandes <fernandofernandesantos@gmail.com>
2021-10-27 00:02:14 -0300

    Update FlexGrip PF faults

--- pf_injector/parse_injection_files.py
+++ pf_injector/parse_injection_files.py
@@ -65,7 +65,7 @@ def parse_lenet_output():
         execute_cmd(f"tar xzf {file} -C {output_folder}/")
         #               fault_0_sa0_DUT_U2086_B_FADD_0_0.txt
         tar_pattern = r"(\d+)_(\S+)_(\S+)_(\d+)_(\d+).tar.gz"
-        m_fault = re.match(tar_pattern, file)
+        m_fault = re.match(tar_pattern, os.path.basename(file))
         # fault_19_sa1_op0_in20_4_27_6_0.tar.gz
         fault_id, fault_location, opcode, lane_id, sm_id = m_fault.groups()
         # IF there is a useful fault to be injected
